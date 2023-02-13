5a5d5bbaf714bb8af4742981f237d5d32ccb65f4
Tianying0902 <Aimee_wang0902@163.com>
2023-02-13 13:18:25 +0800

    [Tianying]feat: header and button

--- front-end-myapp/src/App.tsx
+++ front-end-myapp/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
+        <h1>todos</h1>
+        <button>All</button>
     </div>
   );
 }
