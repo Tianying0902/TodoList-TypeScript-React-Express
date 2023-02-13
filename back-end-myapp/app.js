80896883f163a9d36093cf3e07e14e8ead545dc6
Tianying0902 <Aimee_wang0902@163.com>
2023-02-13 13:54:11 +0800

    [Tianying]feat: connect with mysql

--- back-end-myapp/app.js
+++ back-end-myapp/app.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
-const port = 1234;
+const port = 3001;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  getData(res);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+function mySqlConnection() {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root12345",
+    database: "nodeSql",
+  });
+  return connection;
+}
+function getData(res) {
+  const connection = mySqlConnection();
+
+  connection.connect();
+
+  const selectAllTasks = "SELECT * from todo";
+  connection.query(selectAllTasks, (err, rows) => {
+    if (err) throw err;
+    res.send(rows);
+  });
+
+  connection.end();
+}
