3daf74c8c0e7a5bd78ce3ca2f4032d7d5c3fe893
Tianying0902 <Aimee_wang0902@163.com>
2023-02-13 13:55:51 +0800

    [Tianying]feat: install relevant dependencies

--- front-end-myapp/src/App.tsx
+++ front-end-myapp/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { Todo,ShowAllTodos } from './types';
 
 function App() {
-
+  const [tasks,setTasks] = useState<Array<Todo>>([]);
+const showAllTodos:ShowAllTodos =()=>{
+  axios
+  .get(`http://localhost:3001`)
+  .then((response) => {setTasks(response.data)});
+  console.log(tasks);
+  
+}
   return (
     <div className="App">
         <h1>todos</h1>
-        <button>All</button>
+        <button onClick={showAllTodos}>All</button>
     </div>
   );
 }
