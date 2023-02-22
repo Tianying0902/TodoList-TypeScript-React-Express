14c58c0eecf5738be3808f75d91b10d04bbc6640
Tianying0902 <Aimee_wang0902@163.com>
2023-02-22 14:12:12 +0800

    [Tianying]fix: solve the problem if body-parser is deprecated by setting extended attribute

--- back-end-myapp/app.js
+++ back-end-myapp/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+
 const queryPromise = require("./promise");
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get("/", (req, res) => {
   getData(res);
