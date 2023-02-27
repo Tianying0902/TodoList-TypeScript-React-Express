fc0bd82246c70a3ae6263840ac8a6b4a4e71374a
Tianying0902 <Aimee_wang0902@163.com>
2023-02-27 16:05:07 +0800

    [Tianying]fix: source of tools is updated

--- back-end-myapp/tests/promise.test.js
+++ back-end-myapp/tests/promise.test.js
@@ -1,18 +1,23 @@
-const queryPromise = require("../promise");
-test("fetch data successfully with the content is 'Task 02' ", async () => {
+const queryPromise = require("../api/promise");
+const dbConnection = require("../api/dbConnection");
+
+const connectionMock = jest.fn(dbConnection);
+
+test("make sure connect to database successfully", async () => {
   const selectAllTasks = "SELECT * from todo";
-  const data = await queryPromise(selectAllTasks);
-  const testString = data[0].task;
-  expect(testString).toBe("Task 002");
+  await queryPromise(selectAllTasks, connectionMock);
+  expect(connectionMock.mock.calls.length).toBe(1);
 });
 
 test("the fetch fails with a table that is not exist", async () => {
   //   expect.assertions(1);
   const wrongTableName = "xxx";
   const wrongQuery = "SELECT * from " + wrongTableName + "";
-  return queryPromise(wrongQuery).catch((e) =>
-    expect(e.toString()).toMatch(
-      `ER_NO_SUCH_TABLE: Table 'nodesql.${wrongTableName}' doesn't exist`
-    )
-  );
+  try {
+    await queryPromise(wrongQuery, connectionMock);
+  } catch (err) {
+    expect(err.toString()).toBe(
+      `Error: ER_NO_SUCH_TABLE: Table 'nodesql.xxx' doesn't exist`
+    );
+  }
 });
